2a5c183113cd3c44d1b0c01f03bbabecc316dc50
zubaria <zubariaayub634@gmail.com>
2021-06-14 23:52:40 +0500

    Generates round key 1

--- src/aes/AES.java
+++ src/aes/AES.java
@@ -1,5 +1,7 @@
 package aes;
 
+// A UNIVERSAL ASSUMPTION: all hex numbers in String are on two characters i.e between  and FF inclusive
+
 public class AES {
 
 	private final int n = 4;
@@ -39,13 +41,53 @@ public class AES {
 			{ "0x8C", "0xA1", "0x89", "0x0D", "0xBF", "0xE6", "0x42", "0x68", "0x41", "0x99", "0x2D", "0x0F", "0xB0",
 					"0x54", "0xBB", "0x16" } };
 
-	public void printStringArray(String[][] array) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				System.out.print(array[i][j] + " ");
+	String[][][] allRoundKeys = new String[aesRounds + 1][n][n];
+
+	private void generateAllRoundKeys(String[][] round0Key) {
+		allRoundKeys[0] = round0Key.clone();
+		for (int i = 1; i <= aesRounds; i++) {
+			String[] gw3 = allRoundKeys[i - 1][n - 1].clone();
+			leftShift(gw3);
+			for (int j = 0; j < gw3.length; j++) {
+				gw3[j] = getSubByte(gw3[j]);
 			}
-			System.out.println();
+			gw3[0] = bitwiseXOR(gw3[0], "0x01");
+			String[] w4 = gw3;
+			for (int j = 0; j < w4.length; j++) {
+				w4[j] = bitwiseXOR(w4[j], allRoundKeys[i - 1][0][j]);
+			}
+			allRoundKeys[i][0] = w4.clone();
+			for (int j = 0; j < w4.length; j++) {// w5
+				w4[j] = bitwiseXOR(w4[j], allRoundKeys[i - 1][1][j]);
+			}
+			allRoundKeys[i][1] = w4.clone();
+			for (int j = 0; j < w4.length; j++) {// w6
+				w4[j] = bitwiseXOR(w4[j], allRoundKeys[i - 1][2][j]);
+			}
+			allRoundKeys[i][2] = w4.clone();
+			for (int j = 0; j < w4.length; j++) {// w7
+				w4[j] = bitwiseXOR(w4[j], allRoundKeys[i - 1][3][j]);
+			}
+			allRoundKeys[i][3] = w4.clone();
+						printStringArray(allRoundKeys[i]);
+			break;
 		}
+	}
+
+	private String bitwiseXOR(String hex1, String hex2) {
+		String temp = "0x";
+		temp += Integer.toHexString(hexaToDecimal(hex1.substring(2, 3)) ^ hexaToDecimal(hex2.substring(2, 3)));
+		temp += Integer.toHexString(hexaToDecimal(hex1.substring(3, 4)) ^ hexaToDecimal(hex2.substring(3, 4)));
+		return temp;
+	}
+
+	private void leftShift(String[] array)// one left shift for now
+	{
+		String temp = array[0];
+		for (int i = 0; i < array.length - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		array[array.length - 1] = temp;
 	}
 
 	private Integer hexaToDecimal(String hexa) {
@@ -86,7 +128,7 @@ public class AES {
 	}
 
 	private String[][] getRoundKey(int roundNo) {
-		return new String[n][n]; // TODO: implement
+		return allRoundKeys[roundNo];
 	}
 
 	private String[][] performInitialRound(String[][] currentState, String[][] cipherKey) {
@@ -119,14 +161,30 @@ public class AES {
 		return performFinalRound(temp);
 	}
 
+	public void printStringArray(String[] array) {
+		for (int j = 0; j < n; j++) {
+			System.out.print(array[j] + " ");
+		}
+
+	}
+
+	public void printStringArray(String[][] array) {
+		for (int i = 0; i < n; i++) {
+			printStringArray(array[i]);
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
-		String[][] plainText = { { "0x54", "0x4f", "0x4e", "0x20" }, { "0x77", "0x6e", "0x69", "0x54" },
-				{ "0x6F", "0x65", "0x6e", "0x77" }, { "0x20", "0x20", "0x65", "0x6f" } };
-		String[][] key = { { "0x54", "0x73", "0x20", "0x67" }, { "0x68", "0x20", "0x4b", "0x20" },
-				{ "0x61", "0x6d", "0x75", "0x46" }, { "0x74", "0x79", "0x6e", "0x75" } };
+		String[][] plainText = { { "0x54", "0x77", "0x6f", "0x20" }, { "0x4f", "0x6e", "0x65", "0x20" },
+				{ "0x4e", "0x69", "0x6e", "0x65" }, { "0x20", "0x54", "0x77", "0x6f" } };
+		String[][] key = { { "0x54", "0x68", "0x61", "0x74" }, { "0x73", "0x20", "0x6d", "0x79" },
+				{ "0x20", "0x4b", "0x75", "0x6e" }, { "0x67", "0x20", "0x46", "0x75" } };
 		AES aes = new AES();
-		String[][] cipherText = aes.encrypt(plainText, key);
-		System.out.println("\nCIPHER TEXT:");
-		aes.printStringArray(cipherText);
+		aes.generateAllRoundKeys(key);
+		// String[][] cipherText = aes.encrypt(plainText, key);
+		// System.out.println("\nCIPHER TEXT:");
+		// aes.printStringArray(cipherText);
+
 	}
 }
